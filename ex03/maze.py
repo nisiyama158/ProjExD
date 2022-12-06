2d967bcbe86bfb31634302d6f7bd0157f4fb9fca
nisiyama <c0a21166e1@edu.teu.ac.jp>
2022-12-06 14:50:28 +0900

    3-7

--- ex03/maze.py
+++ ex03/maze.py
@@ -2,7 +2,21 @@ import tkinter as tk
 import tkinter.messagebox as tkm 
 
 def key_down(event):
+    global key
     key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx,cy
+    if key == "Up":cy-=20
+    if key == "Down":cy+=20
+    if key == "Left":cx-=20
+    if key == "Right":cx+=20
+    canvas.coords("koukaton",cx,cy)
+    root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -14,5 +28,7 @@ if __name__ == "__main__":
     cx,cy = 300,400
     canvas.create_image(cx,cy,image=tori,tag="koukaton")
     key = ""
-    root.bind("<keypress>")
+    root.bind("<KeyPress>",key_down)
+    root.bind("<KeyRelease>",key_up)
+    main_proc()
     root.mainloop()
