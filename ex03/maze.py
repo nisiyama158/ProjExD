6e83621beace4a9dc7c2a2404d31b38d1d087bd4
nisiyama <c0a21166e1@edu.teu.ac.jp>
2022-12-06 16:34:42 +0900

    add3 時間の追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -10,6 +10,11 @@ def key_up(event):
     global key
     key = ""
 
+def count_up():
+    global tmr
+    tmr=tmr+1
+    root.after(1000,count_up)
+
 #キーと連動した動き
 def main_proc():
     global cx,cy,mx,my
@@ -27,7 +32,7 @@ def main_proc():
     if cx==1350 and cy==750:
         canvas.create_image(cx,cy,image=santa_neo,tag="santa_neo")
         canvas.delete("santa")
-        tkm.showinfo("ゲームのヒント",f"ゴール！\n遊んでくれてありがとう！")
+        tkm.showinfo("ゲームのヒント",f"ゴール！{tmr}秒でゴールしたよ！\n遊んでくれてありがとう！")
     else:
         root.after(100,main_proc)
 
@@ -44,6 +49,7 @@ if __name__ == "__main__":
     ie=tk.PhotoImage(file="Fig/house_1f.png")
     santa=tk.PhotoImage(file="fig/christmas_santa.png")
     santa_neo=tk.PhotoImage(file="fig/christmas_santa_present.png")
+    tmr=0
     mx,my = 1,1
     cx,cy = mx*100+50,my*100+50
     canvas.create_image(1350,750,image=ie,tag="ie")
@@ -51,5 +57,6 @@ if __name__ == "__main__":
     key = ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
+    count_up()
     main_proc()
     root.mainloop()
