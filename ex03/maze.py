28bf55ddde7512af4dabfb5be8e1fe7975058c8b
nisiyama <c0a21166e1@edu.teu.ac.jp>
2022-12-06 16:07:08 +0900

    add1 画像の変更等

--- ex03/maze.py
+++ ex03/maze.py
@@ -9,6 +9,7 @@ def key_up(event):
     global key
     key = ""
 
+#キーと連動した動き
 def main_proc():
     global cx,cy,mx,my
     if key == "Up":my-=1
@@ -21,22 +22,29 @@ def main_proc():
         if key == "Left":mx+=1
         if key == "Right":mx-=1
     cx,cy = mx*100+50,my*100+50
-    canvas.coords("koukaton",cx,cy)
-    root.after(100,main_proc)
+    canvas.coords("santa",cx,cy)
+    if cx==1350 and cy==750:
+        canvas.create_image(cx,cy,image=santa_neo,tag="santa_neo")
+        canvas.delete("santa")
+    else:
+        root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("サンタが町にやってきた！")
     canvas = tk.Canvas(root,width=1500,height=900,bg="black")
     canvas.pack()
 
     maze_lst = mm.make_maze(15,9)
     mm.show_maze(canvas,maze_lst)
 
-    tori=tk.PhotoImage(file="fig/8.png")
+    ie=tk.PhotoImage(file="Fig/house_1f.png")
+    santa=tk.PhotoImage(file="fig/christmas_santa.png")
+    santa_neo=tk.PhotoImage(file="fig/christmas_santa_present.png")
     mx,my = 1,1
     cx,cy = mx*100+50,my*100+50
-    canvas.create_image(cx,cy,image=tori,tag="koukaton")
+    canvas.create_image(1350,750,image=ie,tag="ie")
+    canvas.create_image(cx,cy,image=santa,tag="santa")
     key = ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
