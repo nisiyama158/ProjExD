f12a3316f40d5888ca9a35f715ca14a013b9673b
nisiyama <c0a21166e1@edu.teu.ac.jp>
2022-12-13 16:22:00 +0900

    爆弾の挙動変更

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -11,6 +11,11 @@ def check_bound(obj_rct,scr_rct):
         tate = -1
     return yoko,tate
 
+def rand_cic():
+    if random.randint(1,1000)>=996:
+        return -1
+    else: return +1
+
 def main():
     clock = pg.time.Clock()
     pg.display.set_caption("逃げろ！こうかとん")
@@ -22,7 +27,7 @@ def main():
     tori_sfc = pg.image.load("fig/6.png") #Scrface
     tori_sfc = pg.transform.rotozoom(tori_sfc,0,2.0)
     tori_rct = tori_sfc.get_rect()
-    tori_rct.center=900,400
+    tori_rct.center=1050,400
     scrn_sfc.blit(tori_sfc,tori_rct) #blit
 
     #爆弾処理
@@ -30,10 +35,10 @@ def main():
     cic_sfc.set_colorkey((0, 0, 0))
     pg.draw.circle(cic_sfc, (255, 0, 0), (10,10), 10)
     cic_rct = cic_sfc.get_rect()
-    cic_rct.centerx = random.randint(0,scrn_rct.width)
+    cic_rct.centerx = random.randint(0,scrn_rct.width/2)
     cic_rct.centery = random.randint(0,scrn_rct.height)
     scrn_sfc.blit(cic_sfc, cic_rct)
-    vx,vy = +1,+1
+    vx,vy = +2,+2
 
     while True:
         
@@ -62,6 +67,8 @@ def main():
                 tori_rct.centerx -= 1
             scrn_sfc.blit(tori_sfc,tori_rct)
 
+        vx *= rand_cic()
+        vy *= rand_cic()
         cic_rct.move_ip(vx,vy)
         scrn_sfc.blit(cic_sfc, cic_rct)
         yoko,tate=check_bound(cic_rct,scrn_rct)
